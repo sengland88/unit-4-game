0c219d5d25b10f47b2c77c668de71b7c6f49e5b7
Shelby England <Shelby.England@knights.ucf.edu>
2019-11-11 19:54:55 -0500

    updating to be mobile responsive

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # unit-4-game
+
+I declared four variable that would hold four random numbers between 1 - 12
+
+i delcared one variable to generate a random number between 19 - 120
+
+i tied an event click function to the page and tied four images to the the four variables holding the random numbers
+
+in each image, i ran a score checker that compared the totals to the computer generated number (between 19 - 120)
+
+the score checker function checked if the total number was either over or matching. if over, it increased losses. if matched, it increased wins. each of these triggered a restart functiuon that reset all random numbers.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,33 @@
-let ranNum = Math.floor(Math.random() * (120 - 19)) + 19;
+// let ranNum = Math.floor(Math.random() * (120 - 19)) + 19;
+// // gen a random number
+// let fireStone = Math.floor(Math.random() * (12 - 1)) + 1;
+// // store it in the  data- attribut
 
+// let waterStone = Math.floor(Math.random() * (12 - 1)) + 1;
+// let thunderStone = Math.floor(Math.random() * (12 - 1)) + 1;
+// let leafStone = Math.floor(Math.random() * (12 - 1)) + 1;
+
+// let totalScore = 0;
+
+// let wins = 0;
+// let losses = 0; 
+
+// console.log(`the random number is ${ranNum}`)
+// console.log(`the fire stone is ${fireStone}`)
+// console.log(`the leaf stone is ${leafStone}`)
+// console.log(`the thunder stone is ${thunderStone}`)
+// console.log(`the water stone is ${waterStone}`)
+
+// $("#gio").text(ranNum)
+// console.log('top', this);
+
+$(document).ready(function() {
+    let ranNum = Math.floor(Math.random() * (120 - 19)) + 19;
+// gen a random number
 let fireStone = Math.floor(Math.random() * (12 - 1)) + 1;
+// store it in the  data- attribut
+$('#char').attr('data-random', fireStone);
+
 let waterStone = Math.floor(Math.random() * (12 - 1)) + 1;
 let thunderStone = Math.floor(Math.random() * (12 - 1)) + 1;
 let leafStone = Math.floor(Math.random() * (12 - 1)) + 1;
@@ -18,31 +45,50 @@ console.log(`the water stone is ${waterStone}`)
 
 $("#gio").text(ranNum)
 
-$(document).ready(function() {
+    console.log('mid', this);
 
-    $("#pika").on("click", function(){
-        totalScore = totalScore + fireStone
-        console.log(totalScore)
+    $(".stone").on("click", () => {
+        console.dir($(this).attr('data-random'));
+        // if (this.id === 'squirt') {
+        //     totalScore = totalScore + waterStone
+        // }
+        // if (this.id === 'pika') {
+        //     totalScore = totalScore + fireStone
+        // }
+        // if (this.id === 'bulba') {
+        //     totalScore = totalScore + leafStone
+        // }
+        // if (this.id === 'char') {
+        //     totalScore = totalScore + thunderStone
+        // }
+        totalScore += Number($(this).attr('data-random'));
         scoreChecker()
     })
+
+
+    // $("#pika").on("click", function(){
+    //     totalScore = totalScore + fireStone
+    //     console.log(totalScore)
+    //     scoreChecker()
+    // })
     
-    $("#squirt").on("click", function(){   
-        totalScore = totalScore + leafStone
-        console.log(totalScore)
-        scoreChecker()
-    })
+    // $("#squirt").on("click", function(){   
+    //     totalScore = totalScore + leafStone
+    //     console.log(totalScore)
+    //     scoreChecker()
+    // })
     
-    $("#bulba").on("click", function(){   
-        totalScore = totalScore + waterStone
-        console.log(totalScore)
-        scoreChecker()
-    })
+    // $("#bulba").on("click", function(){   
+    //     totalScore = totalScore + waterStone
+    //     console.log(totalScore)
+    //     scoreChecker()
+    // })
     
-    $("#char").on("click", function(){    
-        totalScore = totalScore + thunderStone
-        console.log(totalScore)
-        scoreChecker()        
-    })
+    // $("#char").on("click", function(){    
+    //     totalScore = totalScore + thunderStone
+    //     console.log(totalScore)
+    //     scoreChecker()        
+    // })
 
 
     function scoreChecker() {
